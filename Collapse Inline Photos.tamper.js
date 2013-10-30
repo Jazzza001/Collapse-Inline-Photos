a8396fea2384ff679cf20fdb1bd03ee0dfedfdfa
Jared Williams <jared@Jareds-iMac.local>
2013-10-30 09:44:53 +0800

    Initial commit

---
+++ Collapse Inline Photos.tamper.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name       Collapse Inline Photos
+// @namespace  http://www.jaredwilliams.com.au
+// @version    0.1
+// @description  Hides inline Twitter photos from showing in your tweet stream UNTIL you click the tweet.
+// @include      *twitter.com*
+// @copyright  2012+, You
+// @require		//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
+// ==/UserScript==
+
+function addGlobalStyle(css) {
+	var head, style;
+	head = document.getElementsByTagName('head')[0];
+	if (!head) { return; }
+	style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = css;
+	head.appendChild(style);
+}
+
+console.log('Hiding inline twitter photos...');
+
+var css = 
+'.stream-item [data-card-type="photo"] .cards-media-container { display: none } ' + 
+'.stream-item.open [data-card-type="photo"] .cards-media-container { display: block } ';
+
+addGlobalStyle(css);
--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-Collapse-Inline-Photos
+Collapse Inline Photos
 ======================
 
-Userscript to collapse Twitter inline photos.
+A quick and dirty userscript to revert the recent changes to Twitter that shows photos in tweets inline by default.
+
+I expect Twitter to introduce a setting to disable them soon, but until then install this script and they'll disappear. Too easy!
+
+Installing
+----------
+
+- Install your userscript plugin for your favourite modern browser
+- Download and install this script
+- Browse Twitter!
+
+Roadmap
+-------
+
+- Have an idea? Let me know!
